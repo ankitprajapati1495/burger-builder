9579b6bf89275d1a7383b9530738c5cc1348fc49
ankitp <ankitp5@yahoo.com>
2020-12-17 18:34:13 +0530

    interect with server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,33 +95,14 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You Continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Ankit Prajapati",
-    //     address: {
-    //       street: "Katargam",
-    //       zipCode: "395004",
-    //       country: "India",
-    //     },
-    //     email: "ankit@gmail.com",
-    //   },
-    //   deliveryMethod: "fastest",
-    // };
-    // axios
-    //   .post("/orders.json", order)
-    //   .then((responce) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch((error) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
+   
     const queryParams = [];
     for(let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     } 
+
+    queryParams.push('price=' + this.state.totalPrice);
+    
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: "/checkout",
